2ffa476d6c045864a0b982efe652bac548eed40e
maul112 <tsukiaka313@gmail.com>
2025-06-28 20:21:48 +0700

    add state on cerita

--- src/components/Cerita/AlbumFoto.jsx
+++ src/components/Cerita/AlbumFoto.jsx
@@ -1,8 +1,15 @@
+import { useState } from "react";
+
 const AlbumFoto = () => {
+    // isi data gambar disini
     let dataGambar = [];
+    // default selected adalah index 0 dari dataGambar
+    const [selected, setSelected] = useState('https://imgs.search.brave.com/nJ2Beh3qyiTaTjrfza_GczYmTErUqZUnM8-Ufe98y9c/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/cGl4YWJheS5jb20v/cGhvdG8vMjAyMi8w/Mi8xOS8wNi80OS9y/aWNlLWZpZWxkLTcw/MjE5OTdfNjQwLmpw/Zw');
+    // pake foreach buat looping
     for(let i = 0; i < 5; i++) {
         dataGambar.push((
-            <img className="block w-full h-56 object-cover rounded-3xl" src="https://imgs.search.brave.com/nJ2Beh3qyiTaTjrfza_GczYmTErUqZUnM8-Ufe98y9c/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/cGl4YWJheS5jb20v/cGhvdG8vMjAyMi8w/Mi8xOS8wNi80OS9y/aWNlLWZpZWxkLTcw/MjE5OTdfNjQwLmpw/Zw" alt="gambar" />
+            // set state menjadi path gambar dari db
+            <img key={i} className="block w-full h-56 object-cover rounded-3xl" onClick={() => setSelected('https://imgs.search.brave.com/JlDNFjQ22KRN7gB1XrDwE4lMhvSXuXzx6114J2ZkdwQ/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/cGl4YWJheS5jb20v/cGhvdG8vMjAyMi8w/Mi8xMi8wMS81Ni9y/aWNlLWZpZWxkcy03/MDA4MjUyXzY0MC5q/cGc')} src="https://imgs.search.brave.com/nJ2Beh3qyiTaTjrfza_GczYmTErUqZUnM8-Ufe98y9c/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/cGl4YWJheS5jb20v/cGhvdG8vMjAyMi8w/Mi8xOS8wNi80OS9y/aWNlLWZpZWxkLTcw/MjE5OTdfNjQwLmpw/Zw" alt="gambar" />
         ));
     }
     return (
@@ -20,7 +27,7 @@ const AlbumFoto = () => {
                     </div>
                 </div>
                 <div className="w-2/3 pl-6">
-                    <img className="w-full h-full object-cover rounded-4xl" src="https://imgs.search.brave.com/nJ2Beh3qyiTaTjrfza_GczYmTErUqZUnM8-Ufe98y9c/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/cGl4YWJheS5jb20v/cGhvdG8vMjAyMi8w/Mi8xOS8wNi80OS9y/aWNlLWZpZWxkLTcw/MjE5OTdfNjQwLmpw/Zw" alt="gambar" />
+                    <img className="w-full h-full object-cover rounded-4xl" src={selected} alt="gambar" />
                 </div>
             </div>
         </section>
